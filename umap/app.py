e16954e8e86c6faebdf89adb9206348e38a663b1
New-Village <kazuki.niimura@gmail.com>
2019-12-24 19:55:01 +0900

    add collect api

--- umap/app.py
+++ umap/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_restful import Resource, Api
 from datetime import datetime, timedelta
 from controller import races
@@ -12,18 +12,35 @@ def index():
     return render_template('index.html')
 
 
-class Race(Resource):
+class Races(Resource):
     def get(self):
-        return {'HelloWorld': 'GET'}
+        return {'GET Races': 'SUCCESS'}
 
     def post(self):
-        # Collect This Month Races
-        now = datetime.now() + timedelta(weeks=1)
+        # Set Collected Date Parameter
+        if request.form.get("YYYYMM") is not None:
+            now = datetime.strptime(request.form["YYYYMM"], "%Y%m")
+        else:
+            now = datetime.now() + timedelta(weeks=1)
+
+        # Collect Race Data
         msg = races.bulk_collect(now.strftime('%Y'), now.strftime('%m'))
-        return {'POST': str(msg)}
+        return {'POST Races': str(msg)}
 
 
-api.add_resource(Race, '/races')
+class Race(Resource):
+    def get(self, race_id):
+        return {'GET Race': race_id}
+
+    def post(self, race_id):
+        # Collect Race Data
+        msg = races.collect(race_id)
+        return {'POST Race': str(msg)}
+
+
+api.add_resource(Races, '/races')
+api.add_resource(Race, '/races/<string:race_id>')
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True)
