3462beed53b0b2312430850d8396b277542efe47
New-Village <kazuki.niimura@gmail.com>
2019-12-30 11:10:53 +0900

    add bulk collect shell

---
+++ collect.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# #########################################################
+# COLLECT.sh
+# USAGE: ./collect.sh {YEAR:2019}
+# #########################################################
+# USER DEFINE VARIABLES
+HOST=umap.ml
+PORT=5000
+ENTITY=races
+
+# VARIDATE ARGUMENTS
+CMDNAME=`basename $0`
+if [ $# -ne 1 ]; then
+  echo "Usage: ${CMDNAME} YEAR" 1>&2
+  exit 1
+fi
+YEAR=$1
+
+# BULK COLLECT RACE
+for i in `seq 1 12`
+do
+  MONTH=`printf %02d $i`
+  `curl -X POST ${HOST}:${PORT}/${ENTITY} -d "YYYYMM=${YEAR}${MONTH}"`
+done
+
+exit 0
